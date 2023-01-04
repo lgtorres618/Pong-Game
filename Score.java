8e067c5f071f340d5f2b206ce8cfdf4d5cc6cbfa
Luis Guarin <92492074+lgtorres618@users.noreply.github.com>
2023-01-04 16:48:30 -0500

    Create Score.java

---
+++ Score.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class Score {
+    static int GAME_WIDTH;
+    static int GAME_HEIGHT;
+    int player1;
+
+    Score(int GAME_WIDTH, int GAME_HEIGHT) {
+
+    }
+    public void draw(Graphics g){
+
+    }
+
+}
